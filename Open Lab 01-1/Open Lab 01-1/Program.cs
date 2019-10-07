7e0aea6d8fa948a38975216682661bf4e4d921c5
matejslovak <joggurtik1@gmail.com>
2019-10-07 09:44:55 +0200

    Open-Lab-01.1

--- Open Lab 01-1/Open Lab 01-1/Program.cs
+++ Open Lab 01-1/Open Lab 01-1/Program.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_01_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("lklkl");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 10)
+            {
+                Console.WriteLine("This number is less then 10");
+            }
+            else
+            {
+                Console.WriteLine("This number is greater or equal to 10");
+            }
         }
     }
 }
