8a2a72e03d3bc7566200238abd6b662e39fd77c5
matejslovak <joggurtik1@gmail.com>
2019-10-07 09:01:11 +0200

    initial

---
+++ Open Lab 01-1/Open Lab 01-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
